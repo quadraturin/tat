d3daade009eabae01830f8b43db0c64a96b35010
quadra <quadra@quadra.zone>
2024-01-17 13:00:56 -0800

    theme stub in user settings

--- src/lib/userSettings.ts
+++ src/lib/userSettings.ts
@@ -1,1 +1,11 @@
 export let proportionalScaleOnByDefault = false;
+export let invertVolumeScroll = false;
+export let theme = {
+    name: 'warped',
+    color1: 'coral',
+    accent1: 'brown',
+    color2: 'black',
+    accent2: 'gray',
+    color3: 'white',
+    accent3: 'teal'
+}
