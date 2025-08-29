5a922afe5a47b62c2eeee12318c2cf60aae9dae3
quadra <quadra@quadra.zone>
2025-08-28 22:12:25 -0400

    app settings comments cleanup

--- src/lib/settings.appSettings.ts
+++ src/lib/settings.appSettings.ts
@@ -1,45 +1,28 @@
 import { UserSettings } from "./classes/UserSettings.svelte";
 
-/**
- * Valid image file types.
- */
+/** Valid image file types. */
 export const imageFileTypes = ['png', 'gif', 'jpg', 'jpeg', 'webp', 'avif'];
 
-/**
- * Valid sound file types.
- */
+/** Valid sound file types. */
 export const soundFileTypes = ['wav', 'm4a', 'mp3', 'ogg', 'flac'];
 
-/**
- * The project file extension.
- * TODO: remove.
- */
+/** The project file extension. TODO: remove. */
 export const projectExt = "tabletopaudio";
 
-/**
- * The default project name.
- * TODO: move to localization.
- */
+/** The default project name. TODO: move to localization. */
 export const defaultProjectName = "new project";
 
-/**
- * The local sound type.
- */
+/** The local sound type. */
 export const SOUNDTYPE_LOCAL = "local";
 
-/**
- * The global sound type.
- */
+/** The global sound type. */
 export const SOUNDTYPE_GLOBAL = "global";
 
-/**
- * The area sound type.
- */
+/** The area sound type. */
 export const SOUNDTYPE_AREA = "area";
 
-/**
- * The default user settings.
- */
+/** The default user settings. */
 export const defaultUserSettings = new UserSettings;
 
+/** Whether to show debug widgets. */
 export const debugWidgets = false;
