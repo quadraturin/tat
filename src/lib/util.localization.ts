55aeb61fbb99bfcfcd4d2261a7593eb8686414cc
quadra <quadra@quadra.zone>
2025-10-30 15:13:49 -0700

    added ES and PT loc + some loc layout tweaks

--- src/lib/util.localization.ts
+++ src/lib/util.localization.ts
@@ -5,7 +5,8 @@ import { appDataDir, join } from "@tauri-apps/api/path";
 
 // Built-in locales
 import enInfo from '../assets/localization/en/info.json'
-//import esInfo from '../assets/localization/es/info.json'
+import esInfo from '../assets/localization/es/info.json'
+import ptInfo from '../assets/localization/pt/info.json'
 
 // Set up localization config with built-in locales, use English as fallback
 let config = {
@@ -14,6 +15,14 @@ let config = {
         { locale: 'en', key: 'info', loader: async () => (await import ('../assets/localization/en/info.json')).default },
         { locale: 'en', key: 'help', loader: async () => (await import ('../assets/localization/en/help.json')).default },
         { locale: 'en', key: 'ui',   loader: async () => (await import ('../assets/localization/en/ui.json')).default },
+
+        { locale: 'es', key: 'info', loader: async () => (await import ('../assets/localization/es/info.json')).default },
+        { locale: 'es', key: 'help', loader: async () => (await import ('../assets/localization/es/help.json')).default },
+        { locale: 'es', key: 'ui',   loader: async () => (await import ('../assets/localization/es/ui.json')).default },
+
+        { locale: 'pt', key: 'info', loader: async () => (await import ('../assets/localization/pt/info.json')).default },
+        { locale: 'pt', key: 'help', loader: async () => (await import ('../assets/localization/pt/help.json')).default },
+        { locale: 'pt', key: 'ui',   loader: async () => (await import ('../assets/localization/pt/ui.json')).default },
         
         { locale: 'custom', key: 'info', loader: async () => (await getCustomLangFile('info.json')) },
         { locale: 'custom', key: 'help', loader: async () => (await getCustomLangFile('help.json')) },
@@ -22,6 +31,8 @@ let config = {
 };
 
 addLocale({locale:"en", language:enInfo.language});
+addLocale({locale:"es", language:esInfo.language});
+addLocale({locale:"pt", language:ptInfo.language});
 addLocale({locale:"custom", language:"Custom"});
 
 export let { t, locale, locales, loading, loadTranslations } = new I18n(config);
